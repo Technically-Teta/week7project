cea96951d3174b21fcce2aa3d82cce1746cbfacc
tushort <samquioh@gmail.com>
2023-08-30 23:26:49 -0700

    add weather.js componenet

---
+++ client/src/Components/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./weather.css";
+
+function Weather(){
+        return(
+            <div className="weather">
+                <span className="title">Weather App</span>
+
+            </div>
+        )
+
+
+
+}
+
+export default Weather
