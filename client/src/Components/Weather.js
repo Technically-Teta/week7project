1550db3f55065b2fcbb92bf1d0e86a0f6c0a4a0c
tushort <samquioh@gmail.com>
2023-08-31 00:13:26 -0700

    add event handler to weather

--- client/src/Components/Weather.js
+++ client/src/Components/Weather.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
 
 function Weather(){
+    const [weather, setWeather] = useState([]);
+    const[form, setForm]= useState ({  
+        city: "",
+        country: "",
+  });
+
+
+
         return(
             <div className="weather">
-                <span className="title">Weather App</span>
+                <span className="title">Bright Weather App</span>
+                <br/>
+            <form>
+                <input type="text" name="city" placeholder="city"/>
+                &nbsp; &nbsp; &nbsp; &nbsp; 
+            <button className="getweather" >Submit</button>
+            </form>
 
             </div>
         )
