9147d5fcbce4eeb46ddab16b03eb70474acad44e
tushort <samquioh@gmail.com>
2023-08-31 13:24:16 -0700

    add all files to report

--- client/src/Components/Weather.jsx
+++ client/src/Components/Weather.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./weather.css";
+import DisplayWeather from "./DisplayWeather";
 
 function Weather(){
     const APIKEY = '8c03101cdaa9dc6cf93256ee7318adf1'
@@ -19,7 +20,13 @@ async function weatherData(e){
         const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city}&APPID=${APIKEY}`
         ).then(res => console.log(res.json))  // this is a promise
         .then((data) => (data))  //return the data to use and we will log it
-     setWeather(weather);
+     
+        //sets weather state and obj as key value pair
+        setWeather({
+            data :data
+        });
+
+
     }
 }
 
@@ -40,13 +47,36 @@ async function weatherData(e){
             <div className="weather">
                 <span className="title">Bright Weather App</span>
                 <br/>
+             
+             
+
             <form>
                 <input type="text" name="city" placeholder="city" onChange={e => handleChange(e)}/>
                 &nbsp; &nbsp; &nbsp; &nbsp; 
+
+
              <button className="getweather" onClick={(e) => weatherData(e)}>
           Submit
         </button>
+
+             <div className="humidty">
+                <span className="humidyweather">
+                    How humid is it?_ 
+                </span>
+             </div>
+
             </form>
+                   
+               {
+                weather.data != undefined ?
+                <div>
+                    <DisplayWeather data = {weather.data} />
+
+
+                </div>
+                :null
+
+               }
 
             </div>
         )
