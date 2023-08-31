316a3a57de578c0fcdced226a88616e4c3521c01
tushort <samquioh@gmail.com>
2023-08-31 12:05:40 -0700

    add sections of info on display page

--- client/src/Components/DisplayWeather.jsx
+++ client/src/Components/DisplayWeather.jsx
@@ -1,34 +1,134 @@
-import React from 'react'
-import"./displayweather.css"
+import React from "react";
+import "./displayweather.css";
+
 
 function DisplayWeather(props) {
-    const {data} = props
-
-//icons url placeholder    
+  const { data } = props;
+  const iconurl =
+    " " +
+    `${data.code !== 404 ? data.weather[0].icon : null}` +
+    ".png";
   return (
+    <div className="displayweather">
+      {data.cod !== 404 ? (
+        <React.Fragment>
+          <div className="maincard">
+            <span className="cardtitle">
+              {data.name} , {data.sys.city}. Weather
+            </span>
+            <span className="cardsubtitle">
+              As of {new Date().toLocaleTimeString()}
+            </span>
 
-    <div className='displayweather'>  
-    <div className='maincard'> 
-    <span className='cardtitle'> 
-        {data.name}, {data.sys.city}. Weather
-    </span>
-    <span className='cardsubtitle'>
-        As of {new Date().toLocaleTimeString}
-    </span>
-    <h1>
-        data.main.temp
+            <h1>
+              {" "}
+              {Math.floor(data.main.temp )}
+              <sup>o</sup>
+            </h1>
+            <span className="weather-main">{data.weather[0].main}</span>
+            <img className="weather-icon" src={iconurl} alt="" srcSet="" />
+            <span className="weather-description">
+              {" "}
+              {data.weather[0].description}
+            </span>
+          </div>
+          <div className="weatherdetails">
+            <div className="section1">
+              <table>
+              <tbody>
+              <tr>
+                  <td>
+                    <h4>High/Low</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {data.main.temp_max }/
+                      {data.main.temp_min }
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Humidity</h4>
+                  </td>
+                  <td>
+                    <span>{data.main.humidity} %</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Pressure</h4>
+                  </td>
+                  <td>
+                    <span>{data.main.pressure} hPa</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Visibility</h4>
+                  </td>
+                  <td>
+                    <span>{data.visibility / 1000} Km</span>
+                  </td>
+                </tr>
+              </tbody>
+              </table>
+            </div>
 
-    </h1>
-    <span className='weather-main'>   {data.weather[0].main}</span>
-      
-    <img></img>
-    <span className='weather-description'></span>
-      {data.weather[0].description}
-
+            <div className="section2">
+              <table>
+                <tbody>
+                <tr>
+                  <td>
+                    <h4>Wind</h4>
+                  </td>
+                  <td>
+                    <span>{Math.floor((data.wind.speed * 18) / 5)} km/hr</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Wind Direction</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {data.wind.deg}
+                      <sup>o</sup> degrees
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Sunrise</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Sunset</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
+                    </span>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </React.Fragment>
+      ) : (
+        <div className="maincard">
+          <h2>{data.message}</h2>
+        </div>
+      )}
     </div>
-</div>
-
   );
 }
 
-export default DisplayWeather
+export default DisplayWeather;
